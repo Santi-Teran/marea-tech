1afe28d1d02a7dda76f00be0aa84c5b8034eb14b
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-18 20:01:43 -0300

    comit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import web from '../../public/web.svg';
 import { Nunito } from "next/font/google";
+import Link from 'next/link';
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -10,7 +11,7 @@ const Landing = () => {
       <div className="w-1/2 flex flex-col gap-10">
         <h1 className={`${nunito.className} text-6xl font-black`}>Creando olas de innovacion para tu Negocio</h1>
         <h2 className="text-2xl">Soluciones de desarrollo web a medida, con un enfoque personal</h2>
-        <button className={`${nunito.className} text-xl font-black gradient w-fit py-4 px-8 rounded-xl`}>Conoce nuestros servicios</button>
+        <Link href={'/servicios'} className={`${nunito.className} text-xl font-black gradient w-fit py-4 px-8 rounded-xl`}>Conoce nuestros servicios</Link>
       </div>
       <div className="w-1/2">
         <Image src={web} alt=""/>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,5 @@
 import { Nunito } from "next/font/google";
+import Link from "next/link";
 import { FaPenNib, FaFireAlt, FaArrowRight } from "react-icons/fa";
 import { FiMonitor } from "react-icons/fi";
 import { IoIosStats, IoMdCloudUpload, IoMdBusiness } from "react-icons/io";
@@ -13,10 +14,9 @@ const Services = () => {
       <div className="grid grid-cols-3 gap-10 mx-20">
         <div className="flex flex-col shadoww items-center gap-5 p-8 rounded-2xl hover:scale-105 dark:hover:bg-arena dark:hover:text-dark-blue transition-all hover:bg-dark-blue hover:text-arena">
           <div className="bg-verde-agua flex items-center justify-center rounded-full w-fit z-10">
-            <FaFireAlt className="text-8xl p-6" />
+            <Link href={'/servicios'}><FaFireAlt className="text-8xl p-6" /></Link>
           </div>
           <h3 className={`${nunito.className} text-2xl font-bold text-center`}>Idear</h3>
-          <p className="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias reiciendis mollitia, veritatis blanditiis nesciunt, repudiandae molestiae nisi perferendis hic cum error odit asperiores ea, numquam quaerat architecto nostrum distinctio delectus?</p>
           <FaArrowRight className="dark:bg-arena bg-dark-blue text-arena bg-dark dark:text-dark-blue p-2 text-3xl rounded-full" />
         </div>
         <div className="flex flex-col shadoww items-center gap-5 p-8 rounded-2xl hover:scale-105 dark:hover:bg-arena dark:hover:text-dark-blue transition-all hover:bg-dark-blue hover:text-arena">
@@ -24,7 +24,6 @@ const Services = () => {
             <FaPenNib className="text-8xl p-6" />
           </div>
           <h3 className={`${nunito.className} text-2xl font-bold text-center`}>Diseno</h3>
-          <p className="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias reiciendis mollitia, veritatis blanditiis nesciunt, repudiandae molestiae nisi perferendis hic cum error odit asperiores ea, numquam quaerat architecto nostrum distinctio delectus?</p>
           <FaArrowRight className="dark:bg-arena bg-dark-blue text-arena dark:text-dark-blue p-2 text-3xl rounded-full" />
         </div>
         <div className="flex flex-col shadoww items-center gap-5 p-8 rounded-2xl hover:scale-105 dark:hover:bg-arena dark:hover:text-dark-blue transition-all hover:bg-dark-blue hover:text-arena">
@@ -32,7 +31,6 @@ const Services = () => {
             <FiMonitor className="text-8xl p-6" />
           </div>
           <h3 className={`${nunito.className} text-2xl font-bold text-center`}>Desarrollo Web</h3>
-          <p className="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias reiciendis mollitia, veritatis blanditiis nesciunt, repudiandae molestiae nisi perferendis hic cum error odit asperiores ea, numquam quaerat architecto nostrum distinctio delectus?</p>
           <FaArrowRight className="dark:bg-arena bg-dark-blue text-arena dark:text-dark-blue p-2 text-3xl rounded-full" />
         </div>
         <div className="flex flex-col shadoww items-center gap-5 p-8 rounded-2xl hover:scale-105 dark:hover:bg-arena dark:hover:text-dark-blue transition-all hover:bg-dark-blue hover:text-arena">
@@ -40,7 +38,6 @@ const Services = () => {
             <IoIosStats className="text-8xl p-6" />
           </div>
           <h3 className={`${nunito.className} text-2xl font-bold text-center`}>Mantenimiento y Optimizacion</h3>
-          <p className="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias reiciendis mollitia, veritatis blanditiis nesciunt, repudiandae molestiae nisi perferendis hic cum error odit asperiores ea, numquam quaerat architecto nostrum distinctio delectus?</p>
           <FaArrowRight className="dark:bg-arena bg-dark-blue text-arena dark:text-dark-blue p-2 text-3xl rounded-full" />
         </div>
         <div className="flex flex-col shadoww items-center gap-5 p-8 rounded-2xl hover:scale-105 dark:hover:bg-arena dark:hover:text-dark-blue transition-all hover:bg-dark-blue hover:text-arena">
@@ -48,7 +45,6 @@ const Services = () => {
             <IoMdCloudUpload className="text-8xl p-6" />
           </div>
           <h3 className={`${nunito.className} text-2xl font-bold text-center`}>Alojamiento Web</h3>
-          <p className="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias reiciendis mollitia, veritatis blanditiis nesciunt, repudiandae molestiae nisi perferendis hic cum error odit asperiores ea, numquam quaerat architecto nostrum distinctio delectus?</p>
           <FaArrowRight className="dark:bg-arena bg-dark-blue text-arena dark:text-dark-blue p-2 text-3xl rounded-full" />
         </div>
         <div className="flex flex-col shadoww items-center gap-5 p-8 rounded-2xl hover:scale-105 dark:hover:bg-arena dark:hover:text-dark-blue transition-all hover:bg-dark-blue hover:text-arena">
@@ -56,7 +52,6 @@ const Services = () => {
             <IoMdBusiness className="text-8xl p-6" />
           </div>
           <h3 className={`${nunito.className} text-2xl font-bold text-center`}>Consultoria Tecnologica</h3>
-          <p className="text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias reiciendis mollitia, veritatis blanditiis nesciunt, repudiandae molestiae nisi perferendis hic cum error odit asperiores ea, numquam quaerat architecto nostrum distinctio delectus?</p>
           <FaArrowRight className="dark:bg-arena bg-dark-blue text-arena dark:text-dark-blue p-2 text-3xl rounded-full" />
         </div>
       </div>
