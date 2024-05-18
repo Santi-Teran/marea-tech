58cad5ea703fb8470aa4b8fd634e5e0ed23b8125
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-18 18:57:09 -0300

    navbar

--- src/app/components/Landing.jsx
+++ src/app/components/Landing.jsx
@@ -6,7 +6,7 @@ const nunito = Nunito({ subsets: ["latin"] });
 
 const Landing = () => {
   return (
-    <div className="flex justify-center items-center mx-20 my-10 gap-40">
+    <div className="flex justify-center items-center mx-20 my-10 gap-40 mt-28">
       <div className="w-1/2 flex flex-col gap-10">
         <h1 className={`${nunito.className} text-6xl font-black`}>Creando olas de innovacion para tu Negocio</h1>
         <h2 className="text-2xl">Soluciones de desarrollo web a medida, con un enfoque personal</h2>
