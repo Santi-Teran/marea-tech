eb80ecfdd59021d732028fa353035f485c8d41b3
Santi-Teran <santiagoteranmatias@gmail.com>
2024-06-10 11:19:29 -0300

    portafolio

--- src/app/api/projects.js
+++ src/app/api/projects.js
@@ -1,21 +1,24 @@
 const projects = [
   { 
     id: 1,
-    title: "Proyecto 1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "https://th.bing.com/th/id/OIP._joQl3MvICGKwiSWmEpv3QHaDZ?w=1024&h=469&rs=1&pid=ImgDetMain", // Ruta de la imagen del proyecto
+    title: "Turnera",
+    url: "https://turnos-app-frontend.vercel.app/",
+    description: "Simplifica la gestión de turnos y mejora la organización de tu emprendimiento con nuestra Turnera Inteligente. Diseñada para brindar una experiencia fácil e intuitiva tanto para los usuarios como para los dueños de negocios, nuestra aplicación permite a los clientes reservar sus turnos en línea de manera rápida y sencilla. Con funcionalidades avanzadas como verificación de teléfono por SMS, recordatorios automáticos y un panel de administración para gestionar reservas, esta herramienta es ideal para peluquerías, salones de belleza, y otros negocios que dependen de una gestión eficiente de sus citas.",
+    image: "/mockup-turnos.png",
   },
   { 
     id: 2,
     title: "Proyecto 2",
+    url: "https://turnos-app-frontend.vercel.app/",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "/images/project2.jpg", // Ruta de la imagen del proyecto
+    image: "/mockup-turnos.png",
   },
   { 
     id: 3,
     title: "Proyecto 3",
+    url: "https://turnos-app-frontend.vercel.app/",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    image: "/images/project3.jpg", // Ruta de la imagen del proyecto
+    image: "/mockup-turnos.png",
   },
 ];
 
--- src/components/SliderComponent.jsx
+++ src/components/SliderComponent.jsx
@@ -2,48 +2,64 @@
 import React, { useCallback } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
-
+import Image from 'next/image';
+import Link from 'next/link';
 
 const SliderComponent = ({ projects }) => {
-
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true})
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
 
   const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev()
-  }, [emblaApi])
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
   const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext()
-  }, [emblaApi])
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   return (
     <div>
       <div className="flex mx-4 md:mx-20 lg:mx-40 gap-4 md:gap-10">
-        <button className="embla__prev text-3xl md:text-6xl hidden md:block" onClick={scrollPrev}><MdNavigateBefore /></button>
-        <div ref={emblaRef} className='overflow-hidden'>
+        <button className="embla__prev text-3xl md:text-6xl hidden md:block" onClick={scrollPrev}>
+          <MdNavigateBefore />
+        </button>
+        <div ref={emblaRef} className='overflow-hidden w-full'>
           <div className="flex">
             {projects.map((project) => (
-              <div key={project.id} className="embla flex flex-col justify-between w-full md:w-96 bg-verde-agua rounded-3xl p-4 md:p-10 gap-4 md:gap-6">
+              <div key={project.id} className="embla flex flex-col justify-between w-full md:w-96 bg-white rounded-3xl p-4 md:p-10 gap-4 md:gap-6 dark:text-dark-blue">
                 <div className='flex flex-col gap-4'>
-                  <h3 className='text-base md:text-3xl font-bold'>{project.title}</h3>
+                  <Link href={project.url} target='_BLANK' className='hover:text-gray-600'><h3 className='text-base md:text-3xl font-bold'>{project.title}</h3></Link>
                   <hr className='border-2'/>
                   <p className='text-sm md:text-base'>{project.description}</p>
                 </div>
                 <div className='bg-white rounded-xl overflow-hidden'>
-                  <img src={project.image} alt={project.title} className='w-full'/>
+                  <Image 
+                    src={project.image} 
+                    alt={project.title} 
+                    width={1080} 
+                    height={1080} 
+                    layout="responsive"
+                    objectFit="cover"
+                    className='w-full h-auto'
+                  />
                 </div>
               </div>
             ))}
           </div>
         </div>
-        <button className="embla__next text-3xl md:text-6xl hidden md:block" onClick={scrollNext}><MdNavigateNext /></button>
+        <button className="embla__next text-3xl md:text-6xl hidden md:block" onClick={scrollNext}>
+          <MdNavigateNext />
+        </button>
       </div>
       <div className='md:hidden flex justify-center mt-10 gap-20'>
-        <button className="embla__prev text-3xl md:text-6xl" onClick={scrollPrev}><MdNavigateBefore /></button>
-        <button className="embla__next text-3xl md:text-6xl" onClick={scrollNext}><MdNavigateNext /></button>
+        <button className="embla__prev text-3xl md:text-6xl" onClick={scrollPrev}>
+          <MdNavigateBefore />
+        </button>
+        <button className="embla__next text-3xl md:text-6xl" onClick={scrollNext}>
+          <MdNavigateNext />
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default SliderComponent;
