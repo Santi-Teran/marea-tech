7326d21050ae9f072ad27561f19bb9c605193864
Santi-Teran <santiagoteranmatias@gmail.com>
2024-06-10 11:25:50 -0300

    portafolio

--- src/app/api/projects.js
+++ src/app/api/projects.js
@@ -3,7 +3,7 @@ const projects = [
     id: 1,
     title: "Turnera",
     url: "https://turnos-app-frontend.vercel.app/",
-    description: "Simplifica la gestión de turnos y mejora la organización de tu emprendimiento con nuestra Turnera Inteligente. Diseñada para brindar una experiencia fácil e intuitiva tanto para los usuarios como para los dueños de negocios, nuestra aplicación permite a los clientes reservar sus turnos en línea de manera rápida y sencilla. Con funcionalidades avanzadas como verificación de teléfono por SMS, recordatorios automáticos y un panel de administración para gestionar reservas, esta herramienta es ideal para peluquerías, salones de belleza, y otros negocios que dependen de una gestión eficiente de sus citas.",
+    description: "Desarrollamos una turnera online perfecta para gestionar reservas en tu emprendimiento. Con una interfaz intuitiva, permite a tus clientes reservar turnos fácilmente, optimizando la organización de tu negocio. Desde peluquerías hasta centros de estética, nuestra solución se adapta a tus necesidades.",
     image: "/mockup-turnos.png",
   },
   { 
